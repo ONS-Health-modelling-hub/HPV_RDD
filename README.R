0b53cc756e9163046cc8d87c3b7fea4fb670b05b
isobelward <146368920+isobelward@users.noreply.github.com>
2024-11-11 11:05:35 +0000

    Update README.R

--- README.R
+++ README.R
@@ -1,6 +1,6 @@
-## Code for RDD study assessing HPV vaccination 
+## Code for RDD study assessing HPV vaccination - Evaluating the effectiveness of the human papillomavirus vaccination programme in England
 
-#Feb 2024
+#2024
 #I.Ward & C.Bermingham
 
 #To run code you will need the following packages: 
@@ -15,3 +15,7 @@ library(rdrobust)
 
 
 #run scripts sequentially following get_HES_useV3pipe
+
+#code used for the following publications: 
+#ONS: https://www.ons.gov.uk/releases/evaluatingtheeffectivenessofthehumanpapillomavirusvaccinationprogrammeinengland
+#Paper: https://www.medrxiv.org/content/10.1101/2024.04.19.24306076v1
